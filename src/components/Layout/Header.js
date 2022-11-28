98fdfbea8f92eb50d14908284cce672acbb6a40a
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-11-27 23:02:18 -0500

    Initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/header.css";
+
+const header = (props) => {
+  return (
+    <React.Fragment>
+      <header></header>
+      <div></div>
+    </React.Fragment>
+  );
+};
+
+export default header;
