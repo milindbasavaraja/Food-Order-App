ee2bc76dfac1e756cb71b3a2d2df1fa44feffa7e
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-11-28 16:00:39 -0500

    Added Card and Input layout

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./css/header.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCartButton";
 
-const header = (props) => {
+const Header = (props) => {
   return (
     <React.Fragment>
-      <header></header>
-      <div></div>
+      <header className="header">
+        <h1>React Meals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className="main-image">
+        <img src={mealsImage} alt="A table full of delicious food" />
+      </div>
     </React.Fragment>
   );
 };
 
-export default header;
+export default Header;
